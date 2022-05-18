68e911ec54c189dff6df65301d185f87c0697bf4
Erick Won <ecw207@musc.edu>
2022-05-18 15:26:16 -0400

    Update platform_avoidance_training.py
    Finished foundation for ITI, plus clarifications added

--- platform_avoidance_training.py
+++ platform_avoidance_training.py
@@ -14,6 +14,23 @@ class Presets:
     const_CorrectResponse = 3 # Right lever press following the end of the VI timer
     const_ITI = 180 # Mean Intertrial Interval in seconds
 
+# Reference Table at the moment, Will Either Delete Soon or Use to Robustly Integrate Pynapse Settings
+def Channel()
+    class iH10_1:
+        output_Left_Lever_Extension = Channel.1a
+        input_Left_Lever_Press = Channel.2a
+        output_Left_Lever_Light	= Channel.3a
+        output_Reward_Receptacle_Light = Channel.4a
+        output_House_Light = Channel.5a
+        output_Tone = Channel.6a
+        input_Reward_Receptacle_Beam_Break = Channel.7a
+        output_Pellet_Dispenser = Channel.9a
+        output_Shock = Channel.10a
+    class iH10_2:
+        output_Right_Lever_Extension = Channel.1b
+        input_Right_Lever_Press = Channel.2b
+        output_Right_Lever_Light = Channel.3b
+
 #==========================================================#
 #                Program: Preliminary                      #
 #==========================================================#
@@ -65,21 +82,25 @@ def Process_VI_Schedule():
         float_2 = np.random.normal(Presets.const_VISchedule,3,1) # (mean average, standard deviation, amount of numbers)
         float_3 = np.random.normal(Presets.const_VISchedule,3,1) # 3 for 3 VI intervals
         print("The numbers generated are ", VI_Timer.float_1, " ", VI_Timer.float_2, " ", VI_Timer.float_3, " seconds")
+        # VI Schedule Interval 1
         time.sleep(VI_Timer.float_1) # Delay
         def s_1_input_Left_Lever_Press():
             p_Rig.output_Pellet_Dispenser.turnOn()
             p_Rig.output_Pellet_Dispenser.turnOff()
             print('Lever Was Pressed & Logged For Float 1')
+        # VI Schedule Interval 2
         time.sleep(VI_Timer.float_2) # Delay
         def s_2_input_Left_Lever_Press():
             p_Rig.output_Pellet_Dispenser.turnOn()
             p_Rig.output_Pellet_Dispenser.turnOff()
             print('Lever Was Pressed & Logged For Float 2')
+        # VI Schedule Interval 3
         time.sleep(VI_Timer.float_3) # Delay
         def s_3_input_Left_Lever_Press():
             p_Rig.output_Pellet_Dispenser.turnOn()
             p_Rig.output_Pellet_Dispenser.turnOff()
             print('Lever Was Pressed & Logged For Float 3')
+    # Creating Loop For VI Schedule Interval 1,2,3 inside 60 minute timer
     VI_start_time = time.time() # Starting 60 minute timer of the entire VI_Schedule
     VI_seconds = Presets.const_SessionLength
     while True: # Loops Until 60 minute runs out
@@ -102,20 +123,40 @@ def Process_VI_Schedule():
 # Defining ITI Timer Sub-Program
 def Process_ITI_Timer():
     print ("Execute ITI Timer")
+    # Generate Random Number For Timer
     class ITI_Timer:
         import numpy as np
         ITI_Timer_Array = [np.random.normal(Presets.const_ITI,3,100)] # average, standard deviation, amount of numbers
         print("The numbers generated are ", ITI_Timer_Array, "seconds")
-    def ITI_Timer_Loop():
+    # Declaring ITI Experimental Set Up
+    def ITI_Timer_Event_Reg(): # Regular ITI Event
         ITI_Timer = np.random.choice(VI_Timer_Array, size=1)
         time.sleep(ITI_Timer)
+        p_Rig.output_Tone.turnOn() # Starting Tone
+        time.sleep(28)
+        p_Rig.output_Shock.turnOn() # Starting Shock
+        time.sleep(2)
+        p_Rig.output_Shock.turnOff() # Stopping Shock
+        p_Rig.output_Tone.turnOff() # Stopping Tone
+    # Declaring Special Case of ITI Experiment on Third Interval
+    def ITI_Timer_Event_Third():
+        Interblock_Interval = 300 # 5 minutes
+        time.sleep(Interblock_Interval)
+        ITI_Timer = np.random.choice(VI_Timer_Array, size=1) # Redeclare Variable
+    # Wrapping Into 9 Intervals With A Special Instance After Every 3rd ITI Execution
+    def ITI_Intervaling_SetUp():
+        for _ in range(3): # Executing ITI 3 Times
+            ITI_Timer_Event_Reg()
+        ITI_Timer_Event_Third() # Execute Special Case Right After
+    def ITI_Intervaling_Exec(): # Executing ITI_Intervaling_SetUp 3 Times, So ITI is Executed 9 Times
+        for _ in range(3):
+            ITI_Intervaling_SetUp()
+    # Actual Code Execution of ITI
+    ITI_Intervaling_Exec()
 
+# End of ITI Section
 
-
-
-
-
-
+# =================+++++++================= #
 
 # Parallel Execution of Both Programs
 Exec_Process_VI_Schedule = Process(target=Process_VI_Schedule)
