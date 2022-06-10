3d438f18a078eb3fab0b3d14d10d8fae061216c7
Erick Won <ecw207@musc.edu>
2022-06-10 15:24:47 -0400

    Update avoidance_training_new.py

--- Avoidance Training/avoidance_training_new.py
+++ Avoidance Training/avoidance_training_new.py
@@ -70,16 +70,16 @@ class Always:   #StateID = 0
             p_Rig.o_Shock.turnOff()
             print('ITI ', ITI_Ticker,': Tone & Shock Off')
             ITI_Float = int(np.round(np.random.normal(const_ITI,5,1)))
-            ITI_T = ITI_T_30 + ITI_Float
             ITI_Ticker = ITI_Ticker + 1
             print('ITI', ITI_Ticker, ': Using', ITI_Float, '(sec) for', ITI_Ticker, 'ITI interval')
             if 1 < ITI_Ticker <= 9:
                 if ITI_Ticker == 4 or ITI_Ticker == 7:
                     print('Entering an additional 300 second ITI Delay')
-                    ITI_T = ITI_T + 300
+                    ITI_T = ITI_T_30 + ITI_Float + 300
                     ITI_T_28 = ITI_T + 28
                     ITI_T_30 = ITI_T + 30
                 else:
+                    ITI_T = ITI_T_30 + ITI_Float
                     ITI_T_28 = ITI_T + 28
                     ITI_T_30 = ITI_T + 30
             elif ITI_Ticker == 10:
