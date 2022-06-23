a85b1370ec7f0fcb7e9a23ae134181ed0db08d51
Erick Won <ecw207@musc.edu>
2022-06-23 16:26:33 -0400

    Update avoidance_training.py

--- Todd's Experiment/Avoidance Training/avoidance_training.py
+++ Todd's Experiment/Avoidance Training/avoidance_training.py
@@ -15,13 +15,13 @@ import pyopcond_dep as pyop
 
 # Global Static Variables:
 const_ExperimentTime = 3600     # Time of Entire Experiment
-const_VI_Mean = 30           # Variable interval schedule with mean interval (in sec)
+const_VI_Mean = 30              # Variable interval schedule with mean interval (in sec)
 const_VISchedule_Amt = 10       # Amount of VI schedule numbers generated
-const_VICorrectResponse = 3       # Right lever press timeout threshold window following the end of the VI timer (in sec)
+const_VICorrectResponse = 3     # Right lever press timeout threshold window following the end of the VI timer (in sec)
 const_ITISchedule_Amt = 5       # Amount of ITI schedule numbers generated
 const_ITI_Mean = 180            # Mean Inter-Trial Interval (ITI) (in sec)
 const_ITI_Interval = 9          # Amount of ITI
-const_ITI_Add_Delay = 300
+const_ITI_Add_Delay = 300       # Additional ITI Interval Delay
 
 # =================+++++++================= #
 
@@ -31,16 +31,14 @@ VI2_Float = 0
 VI3_Float = 0
 VI_Pool = 0
 VI_Ticker = 1       # Keeps track of how many VI iterations have passed, starts at VI 1 interval
-VI_Latency = 0
-VI_Latency_Start = 0
-VI_Latency_End = 0
+VI_Timer = 0
 # Global Dynamic Variables That Will Change During ITI:
 ITI_Ticker = 0      # Keeps track of how many ITI iterations have passed, starts at ITI 1 interval
 ITI_Float = 0       # Randomly generated number for ITI intervaling, in sec
-ITI_T = 0           # The actual ITI summation value in sec which triggers the tone
+ITI_T = 1           # The actual ITI summation value in sec which triggers the tone
 ITI_Pool = 0
 ITI_Interval = 0
-
+ITI_Switch = 0
 
 #==========================================================#
 #                   Actual Program                         #
@@ -63,21 +61,24 @@ class Always:   #StateID = 0
         '\n', '\n', '\n')
         # Setting Up Global Timer
         print('Global: Setting up Global Timer ')
-        p_Timer.Global_T.setPeriod(1) # Length between ticks (sec)
-        p_Timer.Global_T.setRepeats(const_ExperimentTime) # Amount of ticks
+        p_Timer.Global_T.setPeriod(1)
+        p_Timer.Global_T.setRepeats(const_ExperimentTime)
         print('Global: Starting the global experimental', const_ExperimentTime, ' sec timer')
-        p_Timer.Global_T.start() # Turn on timer
+        p_Timer.Global_T.start()
         # Setting Up VI Timer Schedule via PyOp
         pyop.var_int(const_VISchedule_Amt, const_VI_Mean)
-        VI_Pool = var_int.output_straight
+        VI_Pool = pyop.var_int.output_straight
         print("Global: Generated", VI_Pool, "VI Pool from PyOp")
-        VI1_Float = int(random.choice(VI_Pool))
-        VI2_Float = int(random.choice(VI_Pool))
-        VI3_Float = int(random.choice(VI_Pool))
-        print("Global: The VI numbers generated are ", VI1_Float, " ", VI2_Float, " ", VI3_Float, " seconds", '\n', '\n')
+        while VI1_Float == 0 and VI2_Float == 0 and VI3_Float == 0:
+            VI1_Float = int(random.choice(VI_Pool))
+            VI2_Float = int(random.choice(VI_Pool))
+            VI3_Float = int(random.choice(VI_Pool))
+            if VI1_Float > 0 and VI2_Float > 0 and VI3_Float > 0:
+                print("Global: The VI numbers generated are ", VI1_Float, " ", VI2_Float, " ", VI3_Float, " seconds", '\n', '\n')
+                break
         # Setting Up ITI Timer Schedule via PyOp
         pyop.var_int(const_ITISchedule_Amt, const_ITI_Mean)
-        ITI_Pool = var_int.output_straight
+        ITI_Pool = pyop.var_int.output_straight
         print("Global: Generated:", ITI_Pool, "ITI Pool from PyOp")
         # Switch to PreTrial class
         print('Global: Switching to PreTrial class')
@@ -87,114 +88,96 @@ class Always:   #StateID = 0
         if count == const_ExperimentTime:
             print(const_ExperimentTime, ' sec (60 min) has passed and experiment is completed')
             syn.setModeStr('Idle') # Shuts down Synapse (based on Synapse API)
+        # ITI control triggering
+        if count == ITI_Switch:
+            print('ITI Timer: Initializing the ITI Timer')
+            p_Timer.ITI_T.setPeriod(1)
+            p_Timer.ITI_T.setRepeats(ITI_Switch)
+            p_Timer.ITI_T.start()
+
+    # ===== Conditional Based ITI Scheduling ===== #
+    def s_ITI_T_tick(count):
+        global ITI_Float, ITI_T, ITI_Ticker, ITI_Switch
+        if count == 0:
+            if ITI_Ticker <= const_ITI_Interval:
+                ITI_Ticker = ITI_Ticker + 1
+                ITI_T = 0
+                ITI_Float = 0
+                while ITI_Float == 0:
+                    ITI_Float = int(random.choice(ITI_Pool))
+                    if ITI_Ticker == 4 or ITI_Ticker == 7:
+                        ITI_T = ITI_Float + const_ITI_Add_Delay
+                    break
+                ITI_T = ITI_T + 30
+                p_Timer.ITI_T.setPeriod(1)
+                p_Timer.ITI_T.setRepeats(ITI_T)
+                print('Global: Starting the ITI Timer')
+                p_Timer.ITI_T.start()
+        if count == ITI_T - 30:
+            p_Rig.o_Tone.turnOn()
+            print('ITI Timer', ITI_Ticker, ': Tone On')
+        if count == ITI_T - 28:
+            p_Rig.o_Shock.turnOn()
+            print('ITI Timer', ITI_Ticker, ': Shock On')
+        if count == ITI_T:
+            p_Rig.o_Tone.turnOff()
+            p_Rig.o_Shock.turnOff()
+            print('ITI Timer', ITI_Ticker, ': Shock & Tone Off')
+            ITI_Switch = ITI_T + ITI_Switch
 
 # =================+++++++================= #
 
 class PreTrial:    #StateID = ?
     def s_State_enter():
         # Creates static experimental environment
-        p_Rig.o_House_Light.turnOn() # Turns on light
+        p_Rig.o_House_Light.turnOn()
         print('Pretrial: Light is On')
-        p_Rig.o_L_Lever_Extension.turnOn() # Turns on left lever
+        p_Rig.o_L_Lever_Extension.turnOn()
         print('Pretrial: Lever is Out')
         print('Pretrial: Switching to VI Timer Class')
-        p_State.switch(VI_Timer) # Switches to Trial class
+        p_State.switch(VI_Timer)
 
 # =================+++++++================= #
 
 class VI_Timer:    #StateID = ?
     def s_State_enter():
-        global VI_Ticker, VI_Float
-        VI_Float = 0
+        global VI_Timer
+        VI_Timer = 0
         if VI_Ticker == 1:
-            VI_Float = VI1_Float
-            p_Timer.VI_T.setRepeats(VI_Float)
+            VI_Timer = VI1_Float
+            p_Timer.VI_T.setRepeats(VI1_Float)
             print('VI ', VI_Ticker,' Timer: Initiating timer')
         elif VI_Ticker == 2:
-            VI_Float = VI2_Float
-            p_Timer.VI_T.setRepeats(VI_Float)
+            VI_Timer = VI2_Float
+            p_Timer.VI_T.setRepeats(VI2_Float)
             print('VI ', VI_Ticker,' Timer: Initiating timer')
         elif VI_Ticker == 3:
-            VI_Float = VI3_Float
-            p_Timer.VI_T.setRepeats(VI_Float)
+            VI_Timer = VI3_Float
+            p_Timer.VI_T.setRepeats(VI3_Float)
             print('VI ', VI_Ticker,' Timer: Initiating timer')
-        p_Rig.o_L_Lever_Light.turnOff()
-        print('VI ', VI_Ticker,' Timer: Turned off lever light')
         p_Timer.VI_T.setPeriod(1)
         p_Timer.VI_T.start()
-        print('VI ', VI_Ticker,' Timer: Starting timer')
+        print('VI ', VI_Ticker,' Timer: VI Timer Started')
     def s_VI_T_tick(count):
-        if count == VI_Float:
-            print('VI ', VI_Ticker,' Timer: Interval complete, switching to Event class')
+        if count == VI_Timer:
+            print('VI ', VI_Ticker,' Timer: Interval complete, switching to VI Event class')
             p_State.switch(VI_Event)
 
 class VI_Event:    #StateID = ?
     def s_State_enter():
-        global VI_Latency_Start
-        print('VI ', VI_Ticker,' Event: Starting Latency Measurement')
-        VI_Latency_Start = time.time()
         print('VI ', VI_Ticker,' Event: Entering Event class')
         p_Rig.o_L_Lever_Light.turnOn()
         print('VI ', VI_Ticker,' Event: Left Lever Light On')
     def s_i_L_Lever_Press_rise():
-        global VI_Latency_End, VI_Latency
         print('VI ', VI_Ticker,' Event: Lever was pressed')
         p_Rig.o_Pellet_Dispenser.turnOn()
         time.sleep(1)
         p_Rig.o_Pellet_Dispenser.turnOff()
         print('VI ', VI_Ticker,' Event: Reward dispensed')
-        print('VI ', VI_Ticker,' Event Switching to Reset class')
-        VI_Latency_End = time.time()
-        print('VI ', VI_Ticker,' Event: Ended Latency Measurement')
-        VI_Latency = int(VI_Latency_End - VI_Latency_Start)
-        print('VI ', VI_Ticker,' Event: Latency is', VI_Latency,' sec')
-        print('VI ', VI_Ticker,' Event: Switching to VI to ITI Class')
-        p_State.switch(VI_ITI)
-
-class VI_ITI:   #StateID = ?
-    def s_State_enter():
-        global ITI_Float, ITI_T, ITI_Ticker
-        print('VI ITI: Entered VI to ITI class')
-        if ITI_Ticker < const_ITI_Interval + 1:
-            ITI_Ticker = ITI_Ticker + 1
-            print('VI ITI', ITI_Ticker,': Calculating ITI Timings')
-            ITI_T = 0
-            ITI_Float = 0
-            while ITI_Float =< VI_Float + VI_Latency:
-                ITI_Float = int(random.choice(ITI_Pool))
-                print('VI ITI', ITI_Ticker, ': Chose', ITI_Float, ' sec as the ITI, chekcing ITI parameters')
-                if ITI_Float => VI_Float + VI_Latency:
-                    print('VI ITI', ITI_Ticker, ': Chose', ITI_Float, ' sec for the ITI')
-                    ITI_T = ITI_Float - VI_Float - VI_Latency
-                    print('VI ITI', ITI_Ticker, ': Calculated ', ITI_T, ' sec for the remaining delay')
-                    if ITI_Ticker == 4 or ITI_Ticker == 7:
-                        ITI_T = ITI_T + const_ITI_Add_Delay
-                        print('VI ITI', ITI_Ticker, ': Adding an additional ', const_ITI_Add_Delay, ' sec delay for a', ITI_T, 'sec total delay')
-                    break
-            p_State.switch(ITI_Timer)
-        else:
-            print('VI ITI: ITI Ticker is greater than', const_ITI_Interval, 'sec, switching to VI Timer')
-            p_State.switch(VI_Timer)
-
-class ITI_Timer:  #StateID = ?
-    def s_State_enter():
-        p_Timer.ITI_T.setPeriod(1)
-        p_Timer.ITI_T.setRepeats(ITI_T)
-        p_Timer.ITI_T.start()
-        print('ITI Timer', ITI_Ticker, ': Started the ITI delay timer as ', ITI_T, ' sec')
-    def s_ITI_T_tick(count):
-        if count == ITI_T - 30:
-            print('ITI Timer', ITI_Ticker, ': Tone On')
-            p_Rig.o_Tone.turnOn()
-        if count == ITI_T - 28:
-            print('ITI Timer', ITI_Ticker, ': Shock On')
-            p_Rig.o_Shock.turnOn()
-        if count == ITI_T:
-            print('ITI Timer', ITI_Ticker, ': Shock & Tone Off')
-            p_Rig.o_Tone.turnOff()
-            p_Rig.o_Shock.turnOff()
-            print('ITI Timer', ITI_Ticker, ': Switching to VI Check')
-            p_State.switch(VI_Check)
+        p_Rig.o_L_Lever_Light.turnOff()
+        print('VI ', VI_Ticker,' Event: Lever Lever Light Off')
+        print('VI ', VI_Ticker,' Event: Switching to VI Check Class')
+        p_State.switch(VI_Check)
 
 class VI_Check:   #StateID = ?
     def s_State_enter():
